9c15c98c1a197337732471c346b13bf519da0746
Ryan Gillooly <ryangillooly@hotmail.co.uk>
2019-06-15 04:08:59 +0100

    dd

--- ECMills/App_Start/BundleConfig.cs
+++ ECMills/App_Start/BundleConfig.cs
@@ -9,30 +9,29 @@ namespace ECMills
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Assets/js/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*",
-                        "~/Scripts/jquery.unobtrusive-ajax.js",
-                        "~/Scripts/jquery.unobtrusive-ajax.min.js",
-                        "~/Scripts/jquery.validate.unobtrusive.js",
-                        "~/Scripts/jquery.validate.unobtrusive.min.js"));
+                        "~/Assets/js/jquery.validate*",
+                        "~/Assets/js/jquery.unobtrusive-ajax.js",
+                        "~/Assets/js/jquery.unobtrusive-ajax.min.js",
+                        "~/Assets/js/jquery.validate.unobtrusive.js",
+                        "~/Assets/js/jquery.validate.unobtrusive.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Assets/js/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Assets/js/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Contents/material-dashboard.css?v=2.1.1",
+                      "~/Assets/css/bootstrap.css",
+                      "~/Assets/css/site.css",
+                      "~/Assets/css//material-dashboard.css?v=2.1.1",
                       "~/url/https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons",
                       "~/url/https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"));
-
 
             bundles.Add(new StyleBundle("~/Content/Fonts").Include(
                     "~/url/https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons",
